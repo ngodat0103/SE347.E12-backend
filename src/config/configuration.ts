5ba2de175d8684d3c98b9af8096f09e0e00ff900
Ngô Vũ Minh Đạt <21521935@gm.uit.edu.vn>
2024-09-26 16:05:54 +0700

    saved

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -21,6 +21,6 @@ export default () => ({
     redis: {
       host: process.env.REDIS_HOST,
       port: process.env.REDIS_PORT,
-      password: process.env.REDIS_PASSWORD
+      // password: process.env.REDIS_PASSWORD
     }
   });
